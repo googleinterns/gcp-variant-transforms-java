bb53c2c5f526017fd4feb829707d89a3cd47c43f
Madeline Feigles <madfig@google.com>
2020-06-19 15:45:51 -0700

    tests done for headeriterator's next() and has next()

--- src/test/java/com/google/gcp_variant_transforms/library/HeaderIteratorTest.java
+++ src/test/java/com/google/gcp_variant_transforms/library/HeaderIteratorTest.java
@@ -5,6 +5,7 @@ import static com.google.common.truth.Truth.*;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 import org.junit.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Units tests for HeaderIterator.java
@@ -17,15 +18,91 @@ public class HeaderIteratorTest {
     int listSize = 5;
     ImmutableList.Builder<String> headerLinesBuilder = new ImmutableList.Builder<String>();
     ImmutableList<String> headerLines;
+    HeaderIterator headerIterator;
     for (int i = 0; i < listSize; i++) {
       headerLinesBuilder.add("##sampleHeaderLine=" + i);
     }
     headerLines = headerLinesBuilder.build();
-    HeaderIterator headerIterator = new HeaderIterator(headerLines);
+    headerIterator = new HeaderIterator(headerLines);
     // Act
 
     // Assert
     assertThat(headerIterator.hasNext()).isTrue();
+
+  }
+
+  @Test
+  public void testHeaderLinesHasNext_whenCheckingBoolean_thenFalse() {
+    // Arrange
+    int listSize = 5;
+    ImmutableList.Builder<String> headerLinesBuilder = new ImmutableList.Builder<String>();
+    ImmutableList<String> headerLines;
+    HeaderIterator headerIterator;
+    for (int i = 0; i < listSize; i++) {
+      headerLinesBuilder.add("##sampleHeaderLine=" + i);
+    }
+    headerLines = headerLinesBuilder.build();
+    headerIterator = new HeaderIterator(headerLines);
+    // Act
+    for (int i = 0; i < listSize; i++) {
+      headerIterator.next();
+    }
+    // Assert
+    assertThat(headerIterator.hasNext()).isFalse();
+  }
+
+  @Test
+  public void testHeaderLinesHasNext_whenEmptyCheckingBoolean_thenFalse() {
+    // Arrange
+    int listSize = 5;
+    ImmutableList.Builder<String> headerLinesBuilder = new ImmutableList.Builder<String>();
+    ImmutableList<String> headerLines; 
+    HeaderIterator headerIterator;
+    headerLines = headerLinesBuilder.build(); // empty headerList
+    headerIterator = new HeaderIterator(headerLines);
+    // Act
+
+    // Assert
+    assertThat(headerIterator.hasNext()).isFalse();
+  }
+
+  @Test
+  public void testHeaderLinesNext_whenCheckingString_thenContains() {
+    // Arrange
+    int listSize = 5;
+    CharSequence expectedHeaderLine; 
+    ImmutableList<String> headerLines;
+    HeaderIterator headerIterator;
+    ImmutableList.Builder<String> headerLinesBuilder = new ImmutableList.Builder<String>();
+    for (int i = 0; i < listSize; i++) {
+      headerLinesBuilder.add("##sampleHeaderLine=" + i);
+    }
+    headerLines = headerLinesBuilder.build();
+    headerIterator = new HeaderIterator(headerLines);
+    expectedHeaderLine = "##sampleHeaderLine=0";
+    // Act
+
+    // Assert
+    assertThat(headerIterator.next()).contains(expectedHeaderLine);
+
+  }
+
+  @Test
+  public void testHeaderLinesNext_whenCheckingString_thenException() {
+    // Arrange
+    int listSize = 5;
+    HeaderIterator headerIterator;
+    ImmutableList<String> headerLines;
+    Exception actualException;
+    Class expectedException = IndexOutOfBoundsException.class;
+    ImmutableList.Builder<String> headerLinesBuilder = new ImmutableList.Builder<String>();
+    headerLines = headerLinesBuilder.build(); // empty headerList
+    headerIterator = new HeaderIterator(headerLines);
+    // Act
+    actualException = assertThrows(IndexOutOfBoundsException.class, () ->
+        headerIterator.next());
+    // Assert
+    assertThat(actualException).isInstanceOf(expectedException);
 
   }
 }
