799b2fac576488df7a537810472230d4cb6e8fd6
Madeline Feigles <madfig@google.com>
2020-06-19 11:22:36 -0700

    first test running and passing

--- src/test/java/com/google/gcp_variant_transforms/library/HeaderIteratorTest.java
+++ src/test/java/com/google/gcp_variant_transforms/library/HeaderIteratorTest.java
@@ -2,23 +2,22 @@ package com.google.gcp_variant_transforms.library;
 
 import com.google.gcp_variant_transforms.library.HeaderIterator;
 import static com.google.common.truth.Truth.*;
-import static com.google.common.truth.Truth8.*;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 import org.junit.*;
-import sun.jvm.hotspot.memory.HeapBlock;
 
 /**
  * Units tests for HeaderIterator.java
  */
 public class HeaderIteratorTest {
 
-  pubilc void testHeaderLinesHasNext_whenCheckingBoolean_thenTrue() {
+  @Test
+  public void testHeaderLinesHasNext_whenCheckingBoolean_thenTrue() {
     //arrange
     int listSize = 5;
     ImmutableList.Builder<String> headerLinesBuilder = new ImmutableList.Builder<String>();
     ImmutableList<String> headerLines;
-    for (int i = 0; i < listList; i++) {
+    for (int i = 0; i < listSize; i++) {
       headerLinesBuilder.add("##sampleHeaderLine=" + i);
     }
     headerLines = headerLinesBuilder.build();
