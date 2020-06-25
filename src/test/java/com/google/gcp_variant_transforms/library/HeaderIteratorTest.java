16b3767ccebe298b3f641388930e2d337dbcf31f
Madeline Feigles <madfig@google.com>
2020-06-25 09:21:11 -0700

    took out arrange act assert comments

--- src/test/java/com/google/gcp_variant_transforms/library/HeaderIteratorTest.java
+++ src/test/java/com/google/gcp_variant_transforms/library/HeaderIteratorTest.java
@@ -20,59 +20,43 @@ public class HeaderIteratorTest {
     for (int i = 0; i < numHeaderLines; i++) {
       headerLinesBuilder.add("##sampleHeaderLine=" + i);
     }
-    headerIterator = new HeaderIterator(headerLinesBuilder.build());
-    return headerIterator;
+    return new HeaderIterator(headerLinesBuilder.build());
   }
 
   @Test
   public void testHeaderLinesHasNext_whenCheckingBoolean_thenTrue() {
-    // Arrange
     int numHeaderLines = 5;
     HeaderIterator headerIterator = createHeaderIterator(numHeaderLines);
 
-    // Act
-
-    // Assert
     assertThat(headerIterator.hasNext()).isTrue();
   }
 
   @Test
   public void testHeaderLinesHasNext_whenCheckingBoolean_thenFalse() {
-    // Arrange
     int numHeaderLines = 5;
     HeaderIterator headerIterator = createHeaderIterator(numHeaderLines);
 
-    // Act
     for (int i = 0; i < numHeaderLines; i++) {
       headerIterator.next();
     }
 
-    // Assert
     assertThat(headerIterator.hasNext()).isFalse();
   }
 
   @Test
   public void testHeaderLinesHasNext_whenEmptyCheckingBoolean_thenFalse() {
-    // Arrange
     HeaderIterator headerIterator = new HeaderIterator(ImmutableList.of()); // empty header
 
-    // Act
-
-    // Assert
     assertThat(headerIterator.hasNext()).isFalse();
   }
 
   @Test
   public void testHeaderLinesNext_whenCheckingString_thenContains() {
-    // Arrange
     int numHeaderLines = 5;
     CharSequence expectedHeaderLine1 = "##sampleHeaderLine=0";
     CharSequence expectedHeaderLine2 = "##sampleHeaderLine=1"; 
     HeaderIterator headerIterator = createHeaderIterator(numHeaderLines);
 
-    // Act
-
-    // Assert
     assertThat(headerIterator.next()).contains(expectedHeaderLine1);
     // next should move the pointer
     assertThat(headerIterator.next()).contains(expectedHeaderLine2);
@@ -80,27 +64,19 @@ public class HeaderIteratorTest {
 
   @Test
   public void testHeaderLinesNext_whenCheckingString_thenException() {
-    // Arrange
     HeaderIterator headerIterator = new HeaderIterator(ImmutableList.of()); // empty header
     Class expectedException = IndexOutOfBoundsException.class;
 
-    // Act
-
-    // Assert
     assertThrows(expectedException, () ->
         headerIterator.next());
   }
 
   @Test
   public void testHeaderLinesPeek_whenCheckingString_thenContains() {
-    // Arrange
     int numHeaderLines = 5;
     CharSequence expectedHeaderLine = "##sampleHeaderLine=0"; 
     HeaderIterator headerIterator = createHeaderIterator(numHeaderLines);
 
-    // Act
-
-    // Assert
     assertThat(headerIterator.peek()).contains(expectedHeaderLine);
     // peek shouldn't move the pointer
     assertThat(headerIterator.peek()).contains(expectedHeaderLine); 
@@ -109,13 +85,9 @@ public class HeaderIteratorTest {
 
   @Test
   public void testHeaderLinesPeek_whenCheckingString_thenException() {
-    // Arrange
     HeaderIterator headerIterator = new HeaderIterator(ImmutableList.of()); // empty header
     Class expectedException = IndexOutOfBoundsException.class;
 
-    // Act
-
-    // Assert
     assertThrows(expectedException, () -> 
         headerIterator.peek());
   }
