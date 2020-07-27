0afe862bb21fc6df87ed9bbea425be13282eeecf
Madeline Feigles <madfig@google.com>
2020-07-27 09:00:06 -0700

    VCFHeader generated in parser, stored in context

--- src/main/java/com/google/gcp_variant_transforms/task/VcfToBqTask.java
+++ src/main/java/com/google/gcp_variant_transforms/task/VcfToBqTask.java
@@ -7,6 +7,7 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.gcp_variant_transforms.beam.PipelineRunner;
 import com.google.gcp_variant_transforms.library.HeaderReader;
+import com.google.gcp_variant_transforms.library.VcfParser;
 import com.google.gcp_variant_transforms.options.VcfToBqContext;
 
 import java.io.IOException;
@@ -19,20 +20,24 @@ public class VcfToBqTask implements Task {
   private final HeaderReader headerReader;
   private final PipelineRunner pipelineRunner;
   private final VcfToBqContext context;
+  private final VcfParser parser;
 
   @Inject
   public VcfToBqTask(
         PipelineRunner pipelineRunner,
         HeaderReader headerReader,
-        VcfToBqContext context) throws IOException {
+        VcfToBqContext context,
+        VcfParser parser) throws IOException {
     this.pipelineRunner = pipelineRunner;
     this.headerReader = headerReader;
     this.context = context;
+    this.parser = parser;
   }
 
   @Override
   public void run() throws IOException {
     context.setHeaderLines(headerReader.getHeaderLines());
+    context.setVCFHeader(parser.generateVCFHeader(context.getHeaderLines()));
     pipelineRunner.runPipeline();
   }
 
