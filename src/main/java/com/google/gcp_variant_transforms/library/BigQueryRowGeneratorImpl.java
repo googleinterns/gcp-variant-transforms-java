eb071a075a4ea1ad47257578b96a4e13ba63593f
Zhiqiang Sun <zqsun@google.com>
2020-09-15 16:59:28 -0700

    add malformed message prefix

--- src/main/java/com/google/gcp_variant_transforms/library/BigQueryRowGeneratorImpl.java
+++ src/main/java/com/google/gcp_variant_transforms/library/BigQueryRowGeneratorImpl.java
@@ -24,27 +24,33 @@ public class BigQueryRowGeneratorImpl implements BigQueryRowGenerator, Serializa
     TableRow row = new TableRow();
 
     row.set(Constants.ColumnKeyNames.REFERENCE_NAME, variantContext.getContig());
-    row.set(Constants.ColumnKeyNames.START_POSITION, variantContext.getStart());
+    row.set(Constants.ColumnKeyNames.START_POSITION, variantContext.getStart() - 1);
     row.set(Constants.ColumnKeyNames.END_POSITION, variantContext.getEnd());
 
     row.set(Constants.ColumnKeyNames.REFERENCE_BASES,
-            variantToBqUtils.getReferenceBases(variantContext));
-    row.set(Constants.ColumnKeyNames.NAMES, variantToBqUtils.getNames(variantContext));
+        variantToBqUtils.getReferenceBases(variantContext));
+    try {
+      row.set(Constants.ColumnKeyNames.NAMES, variantToBqUtils.getNames(variantContext));
 
-    // Write alt field and info field to BQ row.
-    List<TableRow> altMetadata = variantToBqUtils.getAlternateBases(variantContext);
-    // AltMetadata should contain Info fields with Number='A' tag, then be added to the row.
-    // The rest of Info fields will be directly added to the base BQ row.
-    variantToBqUtils.addInfo(row, variantContext, altMetadata, vcfHeader, 2);
-    row.set(Constants.ColumnKeyNames.ALTERNATE_BASES, altMetadata);
+      // Write alt field and info field to BQ row.
+      List<TableRow> altMetadata = variantToBqUtils.getAlternateBases(variantContext);
+      // AltMetadata should contain Info fields with Number='A' tag, then be added to the row.
+      // The rest of Info fields will be directly added to the base BQ row.
+      variantToBqUtils.addInfo(row, variantContext, altMetadata, vcfHeader, 2);
+      row.set(Constants.ColumnKeyNames.ALTERNATE_BASES, altMetadata);
 
-    row.set(Constants.ColumnKeyNames.QUALITY, variantContext.getPhredScaledQual());
-    row.set(Constants.ColumnKeyNames.FILTER, variantToBqUtils.getFilters(variantContext));
+      row.set(Constants.ColumnKeyNames.QUALITY, variantContext.getPhredScaledQual());
+      row.set(Constants.ColumnKeyNames.FILTER, variantToBqUtils.getFilters(variantContext));
 
-    // Write calls to BQ row.
-    List<TableRow> callRows = variantToBqUtils.getCalls(variantContext, vcfHeader);
-    row.set(Constants.ColumnKeyNames.CALLS, callRows);
-
+      // Write calls to BQ row.
+      List<TableRow> callRows = variantToBqUtils.getCalls(variantContext, vcfHeader);
+      row.set(Constants.ColumnKeyNames.CALLS, callRows);
+    } catch (Exception e) {
+      String malformedRecordInfoPrefix = "Record with contig: " + variantContext.getContig() + "," +
+          " start position: " + variantContext.getStart() + ", reference bases: " +
+          variantToBqUtils.getReferenceBases(variantContext) + ", error: ";
+      throw new RuntimeException(malformedRecordInfoPrefix + e.getMessage());
+    }
     return row;
   }
 }
